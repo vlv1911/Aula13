6cefde41b5c31c5811bff8496a33bbc1e1aa8b74
vlv1911 <vagnerlvalle@gmail.com>
2026-05-14 21:18:36 -0300

    atualiza repositório

--- aula02/atividade01.py
+++ aula02/atividade01.py
@@ -5,27 +5,27 @@ vd_roupas = pd.read_excel('vendas_roupas.xlsx')
 print(vd_roupas)
 
 print('\nQuantidade total de peças vendidas:')
-print(45 * '=')
+print(110 * '=')
 print(vd_roupas['Unidades Vendidas'].sum())
 
 print('\nMédia de preço dos produtos:')
-print(45 * '=')
+print(110 * '=')
 print(vd_roupas['Preço por Unidade (R$)'].mean())
 
 print('\nProduto com maior faturamento:')
-print(45 * '=')
+print(110 * '=')
 print(vd_roupas['Faturamento Total (R$)'].max())
 
 print('\nProduto com menor faturamento:')
-print(45 * '=')
+print(110 * '=')
 print(vd_roupas['Faturamento Total (R$)'].min())
 
 print('\nProduto com nível de satisfação mais baixo:')
-print(45 * '=')
+print(110 * '=')
 print(vd_roupas[vd_roupas['Satisfação'] == 'Baixo'])
 
 print('\nProduto com faturamento acima da média:')
-print(45 * '=')
+print(110 * '=')
 media = vd_roupas['Faturamento Total (R$)'].mean()
 print(vd_roupas[vd_roupas['Faturamento Total (R$)'] >= media])
 
